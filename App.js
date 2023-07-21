e2cef7d5f0f1daaa39d007bb39ded1122a4a4a02
PHKhande <phk151raj@gmail.com>
2023-07-22 02:58:25 +0530

    Creating our own react app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement("h1", {id:"head1"}, "Hello World!");
 const heading2 = React.createElement("h2", {id:"head2"}, "Hello World 2!");
 const container = React.createElement("div", {id:"container"}, [heading1, heading2]);
