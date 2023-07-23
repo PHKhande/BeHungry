9d997d4b60ccd32168a4f931287abe1d835885f2
PHKhande <phk151raj@gmail.com>
2023-07-24 01:32:02 +0530

    Starting with JSX with babel

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {id:"head1"}, "Hello World!");
-const heading2 = React.createElement("h2", {id:"head2"}, "Hello World 2!");
-const container = React.createElement("div", {id:"container"}, [heading1, heading2]);
+import logo from "./util/Logo.png";
+
+// React.createElement() => obj => HTML(DOM)
+// Below line is known as React Element
+const heading1 = React.createElement(
+  "h1",
+  { id: "head1", key: "h1" },
+  "Hello World!"
+);
+
+// JSX => React.createElement() => obj => HTML(DOM)
+// Below line is known as JSX Expression
+const heading2 = (
+  <h2 id="head2" key="h2">
+    Hello World 2!
+  </h2>
+);
+
+//Name has to be in UpperCamelCase
+//Below is a functional component
+const Heading3 = function () {
+  return (
+    <h3 id="head3" key="h3">
+      Hello World 3!
+    </h3>
+  );
+};
+
+const Heading4 = function () {
+  return (
+    <h3 id="head4" key="h4">
+      Hello World 4!
+    </h3>
+  );
+};
+
+//Component Composition : Component inside component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <img src={logo} />
+      <input
+        className="input"
+        type="text"
+        placeholder="Search anything you want..."
+      />
+      {heading1}
+      {heading2}
+      <Heading3 />
+      {Heading4()}
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeadingComponent />);
